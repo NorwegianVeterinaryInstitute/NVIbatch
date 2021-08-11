6d3d65435a667b2db60e7fc99ed61a3deca54e07
Petter Hopp <Petter.Hopp@vetinst.no>
2021-08-11 13:55:04 +0200

    chore: build.R code for generating CONTRIBUTING.md

--- notes/build.R
+++ notes/build.R
@@ -9,11 +9,25 @@ Rlibrary <- R.home()
 
 library(devtools)
 library(roxygen2)
-library(withr)
 
 # Creates new help files
 # Should be run before git push when documentation for functions have been changed
 devtools::document()
+
+# # For updating README.md when the Rmd-file has been updated.
+# rmarkdown::render(input = paste0("./README.Rmd"),
+#                   # output_format = "md_document",
+#                   output_file = "README.md",
+#                   output_dir = "./")
+
+# For updating CONTRIBUTE.md when the vignette has been updated.
+rmarkdown::render(input = paste0("./vignettes/Contribute_to_", pkg, ".Rmd"),
+                  output_format = "md_document",
+                  output_file = "CONTRIBUTING.md",
+                  output_dir = "./")
+header <- paste0("# Contribute to ", pkg, "\n",
+                 "<!-- CONTRIBUTING.md is generated from ./vignettes/Contribute_to_", pkg, ".Rmd. Please edit that file -->", "\n")
+writeLines(c(header,readLines("./CONTRIBUTING.md")),"./CONTRIBUTING.md")
 
 # Run tests included in ./tests.
 devtools::test()
